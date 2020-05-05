079c91ddca11f3a6e710d8ecb0c1c96e5fe14d7d
Tallerik <erik.klasse@gmail.com>
2020-05-05 21:39:28 +0200

    Fixed manifest reader

--- src/main/java/de/tallerik/fivem/documenter/generator/GeneratorRunner.java
+++ src/main/java/de/tallerik/fivem/documenter/generator/GeneratorRunner.java
@@ -1,7 +1,9 @@
 package de.tallerik.fivem.documenter.generator;
 
 import de.tallerik.fivem.documenter.generator.types.DocsTemplate;
+import de.tallerik.fivem.documenter.reader.types.ReaderObject;
 import de.tallerik.fivem.documenter.reader.types.ReaderPackage;
+import de.tallerik.fivem.documenter.reader.types.ReaderResource;
 import javafx.util.Pair;
 
 public class GeneratorRunner {
@@ -9,13 +11,26 @@ public class GeneratorRunner {
     public static Pair<String, String> generate(ReaderPackage pack, DocsTemplate template) {
         String sidebar = "";
         String content = "";
+        Pair<String, String> output = new Pair<String, String>(sidebar, content);
 
 
 
 
 
 
-        return new Pair<String, String>(sidebar, content);
+        return output;
     }
 
+    public Pair<String, String> loopThrow(ReaderPackage pack, Pair<String, String> data) {
+        for (ReaderObject object : pack.getObj()) {
+            if(object instanceof ReaderResource) {
+
+            } else {
+
+            }
+        }
+
+
+        return data;
+    }
 }
--- src/main/java/de/tallerik/fivem/documenter/reader/types/ReaderResource.java
+++ src/main/java/de/tallerik/fivem/documenter/reader/types/ReaderResource.java
@@ -101,45 +101,45 @@ public class ReaderResource implements ReaderObject {
 
             // fx_version
             if(line.startsWith("fx_version")) {
-                if(line.split("'")[1] != null) {
+                if(line.split("'").length > 1) {
                     properties.put("manifest_version", line.split("'")[1]);
-                } else if(line.split("\"")[1] != null) {
+                } else if(line.split("\"").length > 1) {
                     properties.put("manifest_version", line.split("\"")[1]);
                 }
             }
 
             // resource_manifest_version
             if(line.startsWith("resource_manifest_version")) {
-                if(line.split("'")[1] != null) {
+                if(line.split("'").length > 1) {
                     properties.put("manifest_version", line.split("'")[1]);
-                } else if(line.split("\"")[1] != null) {
+                } else if(line.split("\"").length > 1) {
                     properties.put("manifest_version", line.split("\"")[1]);
                 }
             }
 
             // author
             if(line.startsWith("author")) {
-                if(line.split("'")[1] != null) {
+                if(line.split("'").length > 1) {
                     properties.put("author", line.split("'")[1]);
-                } else if(line.split("\"")[1] != null) {
+                } else if(line.split("\"").length > 1) {
                     properties.put("author", line.split("\"")[1]);
                 }
             }
 
             // description
             if(line.startsWith("description")) {
-                if(line.split("'")[1] != null) {
+                if(line.split("'").length > 1) {
                     properties.put("description", line.split("'")[1]);
-                } else if(line.split("\"")[1] != null) {
+                } else if(line.split("\"").length > 1) {
                     properties.put("description", line.split("\"")[1]);
                 }
             }
 
             // data file
             if(line.startsWith("data_file")) {
-                if(line.split("'")[1] != null) {
+                if(line.split("'").length > 1) {
                     dataFiles.put(line.split("'")[1], new File(basePath + sep + line.split("'")[3]));
-                } else if(line.split("\"")[1] != null) {
+                } else if(line.split("\"").length > 1) {
                     dataFiles.put(line.split("\"")[1], new File(basePath + sep + line.split("'")[3]));
                 }
             }
@@ -149,9 +149,12 @@ public class ReaderResource implements ReaderObject {
                 if(line.endsWith("{")) {
                     bracket = "client";
                 } else {
-                    if(line.split("'")[1] != null) {
+                    if(line.contains("*") || !line.contains(".lua")) {
+                        continue;  // TODO: Workaround for this
+                    }
+                    if(line.split("'").length > 1) {
                         clientLua.add(new LuaFile(basePath + sep + line.split("'")[1], ScriptType.CLIENT_SCRIPT));
-                    } else if(line.split("\"")[1] != null) {
+                    } else if(line.split("\"").length > 1) {
                         clientLua.add(new LuaFile(basePath + sep + line.split("\"")[1], ScriptType.CLIENT_SCRIPT));
                     }
                 }
@@ -162,12 +165,13 @@ public class ReaderResource implements ReaderObject {
                 if(line.endsWith("{")) {
                     bracket = "server";
                 } else {
-                    if(line.contains("*")) {
+                    if(line.contains("*") || !line.contains(".lua")) {
                         continue;  // TODO: Workaround for this
                     }
-                    if(line.split("'")[1] != null) {
+                    if(line.split("'").length > 1) {
+                        System.out.println(line);
                         serverLua.add(new LuaFile(basePath + sep + line.split("'")[1], ScriptType.SERVER_SCRIPT));
-                    } else if(line.split("\"")[1] != null) {
+                    } else if(line.split("\"").length > 1) {
                         serverLua.add(new LuaFile(basePath + sep + line.split("\"")[1], ScriptType.SERVER_SCRIPT));
                     }
                 }
@@ -178,12 +182,12 @@ public class ReaderResource implements ReaderObject {
                 if(line.endsWith("{")) {
                     bracket = "shared";
                 } else {
-                    if(line.contains("*")) {
+                    if(line.contains("*") || !line.contains(".lua")) {
                         continue;  // TODO: Workaround for this
                     }
-                    if(line.split("'")[1] != null) {
+                    if(line.split("'").length > 1) {
                         sharedLua.add(new LuaFile(basePath + sep + line.split("'")[1], ScriptType.SHARED_SCRIPT));
-                    } else if(line.split("\"")[1] != null) {
+                    } else if(line.split("\"").length > 1) {
                         sharedLua.add(new LuaFile(basePath + sep + line.split("\"")[1], ScriptType.SHARED_SCRIPT));
                     }
                 }
@@ -194,10 +198,13 @@ public class ReaderResource implements ReaderObject {
                 if(bracket == null) {
                     continue;
                 }
+                if(!line.contains(".lua")) {
+                    continue;
+                }
                 String name = "";
-                if(line.split("'")[1] != null) {
+                if(line.split("'").length > 1) {
                     name = line.split("'")[1];
-                } else if(line.split("\"")[1] != null) {
+                } else if(line.split("\"").length > 1) {
                     name = line.split("\"")[1];
                 }
                 if(name.equals("") || name.contains("*")) {
